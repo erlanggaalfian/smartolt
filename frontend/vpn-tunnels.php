19c144caac5d4bd197e76b34770c1f130f2085c2
Erlangga Alfian <erlanggaalfian@users.noreply.github.com>
2026-09-21 23:44:04 +0700

    VPN popup: cleanup di dalam script, delay 5s, cukup 2 baris paste

--- frontend/vpn-tunnels.php
+++ frontend/vpn-tunnels.php
@@ -327,11 +327,11 @@ function showMikrotik(id) {
     const url = location.origin + '/vpn/' + id + '/' + apiToken + '.rsc';
     let rscName = 'setup-VPN-Erlangga-SmartOLT-' + t.username + '.rsc';
     let fetchCmd = '/tool fetch url="' + url + '" dst-path=' + rscName;
-    let importCmd = '/import ' + rscName;
+    let importCmd = ':delay 3s\n/import ' + rscName;
+    let cleanupCmd = ':delay 5s\n/system script remove vpn-setup';
     let script = '# Paste di Terminal MikroTik (satu kali paste):\n\n';
-    script += '/system script add name=vpn-setup source="' + fetchCmd + '\\n' + importCmd + '"\n';
-    script += '/system script run vpn-setup\n';
-    script += '/system script remove vpn-setup';
+    script += '/system script add name=vpn-setup source="' + fetchCmd + '\\n' + importCmd + '\\n' + cleanupCmd + '"\n';
+    script += '/system script run vpn-setup';
     document.getElementById('mt-script').value = script;
     document.getElementById('mt-modal').classList.add('open');
 }
